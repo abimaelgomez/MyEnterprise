68f0e9c33f8af24a3b3ee9e801c2a18e0eb16a53
Abimael <abimaelcarvalho95@gmail.com>
2023-09-09 13:23:38 -0300

    feat: initial commit

---
+++ src/main/java/com/br/MyEnterprise/MyEnterpriseApplication.java
@@ -0,0 +1,13 @@
+package com.br.MyEnterprise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyEnterpriseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyEnterpriseApplication.class, args);
+	}
+
+}
